8c2e970afa5d6bf25762f7381f2f7a510b7c81c4
progmae20 <69871765+progmae20@users.noreply.github.com>
2024-07-24 20:40:19 +0300

    Create LoginApp.swift

---
+++ Login/Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Anastasia on 24.07.24.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
